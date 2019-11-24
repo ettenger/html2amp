03409426d19ef5d50723ba8c87c869b0ab1ec585
Andrew Ettenger <ettenger@gmail.com>
2019-11-23 20:33:26 -0500

    Add page parsing function

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 $ npm i -g serverless
 $ npm run dev
 ```
+
+### Try parsing something
+http://localhost:3000/parse?url=https://www.mrmoneymustache.com/2013/04/22/curing-your-clown-like-car-habit/
+
+Based on the (MIT-licensed) Mercury Parser: https://github.com/postlight/mercury-parser
+Which they use to power their own AMP converter: https://mercury.postlight.com/amp-converter/
--- handler.ts
+++ handler.ts
@@ -1,12 +1,30 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import Mercury from '@postlight/mercury-parser';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+export const parse: APIGatewayProxyHandler = async (event) => {
+  const params = event.queryStringParameters;
+  if (!params || !params.url) {
+    return {
+      statusCode: 400,
+      body: 'Missing url'
+    };
+  }
+
+  try {
+    const parsedPage = await Mercury.parse(params.url);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(parsedPage)
+    };
+
+  } catch (e) {
+    console.log(e);
+
+    return {
+      statusCode: 500,
+      body: 'Error parsing page'
+    };
+  }
 }
